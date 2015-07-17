080cf9b18ba4976f229f273c44a1b6435a971557
Brian Norton <brian.nort@gmail.com>
2015-07-17 12:43:18 -0700

    Show how to setup Chameleon on ajax complete.

---
+++ examples/chameleon-ajax.js
@@ -0,0 +1,21 @@
+// Add the snippet here with account id (i.e. config.chameleonAccountId)
+// Assuming you call `currentUserLoaded` after fetching the user
+
+(function() {
+  var currentUserLoaded = function(currentUser) {
+    chmln.setup({
+      uid: currentUser.id,
+      created: currentUser.createdAt,
+      email: currentUser.email,
+      plan: currentUser.planName,
+      spend: currentUser.planCost
+    });
+  };
+
+  var xhr = $.get('/user.json');
+  xhr.done(function(data) {
+    // Setup other aspects of the environment
+
+    currentUserLoaded(data.user);
+  });
+})();
