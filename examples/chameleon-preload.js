72b9f104d817c471b2a38c8271015917fc64f440
Brian Norton <brian.nort@gmail.com>
2015-07-17 12:43:42 -0700

    Show how to setup Chameleon if the user is preloaded into the page.

---
+++ examples/chameleon-preload.js
@@ -0,0 +1,14 @@
+// Add the snippet here with account id (i.e. config.chameleonAccountId)
+// Assuming you preload your page with a current user
+
+(function() {
+  if(currentUser.id) {
+    chmln.setup({
+      uid: currentUser.id,
+      created: currentUser.createdAt,
+      email: currentUser.email,
+      plan: currentUser.planName,
+      spend: currentUser.planCost
+    });
+  }
+})();
