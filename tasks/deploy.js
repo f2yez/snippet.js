06de00045e9a3956a06fa6cb8a736b185b3b9d84
Aaron Cody <aaron.cody@gmail.com>
2017-06-21 12:36:16 -0700

    remove publish to hyoid

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -20,34 +20,4 @@
 
   console.log('Gzipped to: '+Buffer.byteLength(gzippedBody.toString())/1024+'KB');
 
-  var requestOptions = {
-    method: 'POST',
-    body: gzippedBody,
-    headers: {
-      'X-Api-Secret': process.env.HYOID_SECRET || 'local-hyoid-secret',
-      'Content-Type': 'application/octet-stream',
-      'Content-Encoding': 'gzip'
-    }
-  };
-
-  branch === 'master' && (branch = null);
-
-  'nyc2-1,sf1-1,ams2-1,sng1-1'.split(',').
-    forEach(function(host) {
-      requestOptions.url = 'https://hyoid-'+host+'.trychameleon.com/scripts/snippet?revision='+branch;
-
-      request(requestOptions, onCompleteFactory('Hyoid:'+host));
-    });
-
-  function onCompleteFactory(name) {
-    return function(err) {
-      if(err) {
-        console.log('Error: snippet:'+name);
-
-        throw err;
-      }
-
-      console.log('Upload of snippet:'+name+' completed in ' + (new Date().getTime() - startTime) + 'ms');
-    }
-  }
 })();
